ac09f1e27f706b813a36162ca96a4d16bf58d3a9
bloop <watdahecklol32@gmail.com>
2026-05-24 08:50:19 +0000

    plz

--- Uranium/Closure.hpp
+++ Uranium/Closure.hpp
@@ -6,7 +6,7 @@ namespace Uranium
     int islclosure(lua_State* lua_state_ptr);
     int clonefunction(lua_State* lua_state_ptr);
     int newcclosure(lua_State* lua_state_ptr);
-    int newlclosure(lua_State* L);
+    int newlclosure(lua_State* lua_state_ptr);
     int hookfunction(lua_State* lua_state_ptr);
     int isfunctionhooked(lua_State* lua_state_ptr);
     int restorefunction(lua_State* lua_state_ptr);
